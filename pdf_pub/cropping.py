a338cc946405f5ae68a12b05ac6c42aae1941685
Knudn <kor@kroland.no>
2023-02-04 22:11:55 +0000

    Removed unused cropping script

--- pdf_pub/cropping.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-img = Image.open('/share/Skjerm3/TopFuelStartlisteRun1.pdf.jpg')
-img.show()
-box = (0, 0, 1875, 1300)
-img2 = img.crop(box)
-img2.save('/share/Skjerm3/TopFuelStartlisteRun1_new_.pdf.jpg')
-img2.show()
-
