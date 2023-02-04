91006e9cb2428a7b2b9b7bd108a7623698f96ef7
Knudn <kor@kroland.no>
2023-02-04 22:10:57 +0000

    Added a bunch of stuff

--- setup_smb.sh
+++ setup_smb.sh
@@ -8,10 +8,14 @@ chmod -R 777 /share
 if grep -wq "SMB_SHARE" /etc/samba/smb.conf; then 
     echo "Exists" 
 else 
-    echo -en "[myshare]\n   comment = SMB_SHARE\n   path = /share\n   read only = No\n" >> /etc/samba/smb.conf
+    echo -en "[PDF share]\n   comment = SMB_SHARE\n   path = /share\n   read only = No\n" >> /etc/samba/smb.conf
 fi
 
 #User for SMB
 #Username will be root
 pass="secret"
 echo -e "$pass\n$pass" | smbpasswd -a -s $(id -un)
+
+systemctl stop smbd
+systemctl start smbd
+systemctl enable smbd
